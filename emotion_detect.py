02797c4867ae52d8a779fddceb0cfa080e55e5d1
Shankho Boron Ghosh <shankhoghosh123@gmail.com>
2022-12-24 14:51:17 +0100

    Emotion Recognition on PiCamera
    32 bit Raspian OS on RPi 4
    
    Dependencies:
    sudo apt-get install libatlas-base-dev
    pip3 install -U numpy

--- emotion_detect.py
+++ emotion_detect.py
@@ -7,29 +7,30 @@ import requests
 from config import API_KEY, API_SECRET, API, DEBUG
 import patient
 from patient import Patient
-
+import cv2
+from picamera import PiCamera
 
 def emotion_mapper(pat, emotion_list):
     '''
-    returns the emotion with the highest confidence
+    returns the emoji with the highest confidence
     '''
-    return pat.mapper(max(emotion_list, key=emotion_list.get))
+    emotion = max(emotion_list, key=emotion_list.get)
+    return emotion, pat.mapper(emotion)
 
-
-'''def take_photo():
+def take_photo():
     # take a photo with the camera
     # camera = cv2.VideoCapture(0) # for testing purposes
     camera = PiCamera()
     camera.start_preview()
-    camera.resolution = (640, 480)
+    camera.resolution = (160, 120)
+    time.sleep(3)
     camera.capture('images/face.jpeg')
-    time.sleep(15)'''
 
 
 def emotion_detect():
     while True:
         # real-time emotion detection with the R-Pi
-        '''# take a photo
+        # take a photo
         take_photo()
         # encode the image
         with open('images/face.jpeg', 'rb') as image_file:
@@ -37,10 +38,13 @@ def emotion_detect():
         # send the image to the API and get the response
         response = requests.post(API, data={'api_key': API_KEY, 'api_secret': API_SECRET, 'image_base64': encoded_image},
                                  params={'return_attributes': 'emotion'})
+        faceID = emotions = response.json().get('faces')[0].get('face_token') #Used for number of people met
         emotions = response.json().get('faces')[0].get('attributes').get('emotion')
-        emoticon = emotion_mapper(pat, emotions)
-        print(f'timestamp: {emoticon[0]}, Emoji: {emoticon[1]}')'''
+        emotion, emoticon = emotion_mapper(pat, emotions)
+        #print(response.json())
+        print(f'timestamp: {emoticon[0]}, Emotion: {emotion}, Emoji: {emoticon[1]}, FaceID: {faceID}')
 
+        '''
         # for testing purposes
         for image in encoded_images:
             response = requests.post(API, data={'api_key': API_KEY, 'api_secret': API_SECRET, 'image_base64': image},
@@ -50,10 +54,10 @@ def emotion_detect():
             # get only the emotions data for the first face detected
 
             emotions = response.json().get('faces')[0].get('attributes').get('emotion')
-            emoticon = emotion_mapper(pat, emotions)
-            print(f'timestamp: {emoticon[0]}, Emoji: {emoticon[1]}')
+            emotion, emoticon = emotion_mapper(pat, emotions)
+            print(f'timestamp: {emoticon[0]}, Emotion: {emotion}, Emoji: {emoticon[1]}, FaceID: {faceID}')
             #time.sleep(5)
-
+        '''
 
 thread = Thread(target=emotion_detect)
 
@@ -69,9 +73,11 @@ def encode_image(image):
 
 
 encoded_images = []
+
 for file in os.listdir('images'):
     if file.endswith('.jpeg' or '.png' or '.jpg'):
         with open(os.path.join('images', file), 'rb') as image_file:
             encoded_images.append(encode_image(image_file.read()))
+
 thread.start()
 #if DEBUG: print(f'\nEnded.\nTime of execution: {time.time() - start:.2} s')
