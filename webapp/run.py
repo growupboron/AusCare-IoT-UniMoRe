2f710bb37ed55f75ca74e3a34d9a070b8bf0dbb4
Shankho Boron Ghosh <shankhoghosh123@gmail.com>
2023-01-10 00:41:49 +0100

    Added ngrok support for local <-> global tunnel

--- webapp/run.py
+++ webapp/run.py
@@ -1,8 +1,10 @@
-
 from website import create_app
+from flask_ngrok2 import run_with_ngrok
 
 app = create_app()
-print(__name__)
-if __name__ == '__main__':
+run_with_ngrok(app)
 
-    app.run(debug=True)
+print(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000)
