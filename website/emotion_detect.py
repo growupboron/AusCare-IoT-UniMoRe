9e01ff4c683cdb74acce6fa7a3ce535fc175b87c
MicheleBellitti <michelebellitti272@gmail.com>
2023-01-16 16:10:51 +0100

    Patient changes

--- website/emotion_detect.py
+++ website/emotion_detect.py
@@ -150,7 +150,7 @@ try:
             cur.execute("SELECT * FROM user WHERE role = 'User' ORDER BY id DESC")
             last_user = cur.fetchone()
             conn.close()
-            pat = Patient(patient_id=last_user[0], name=last_user[3])
+            pat = Patient(patient_id=100, user_id=last_user[0], name=last_user[3])
         pat.load()
         # print all the attributes of the patient
         #print(pat.__dict__)
